ba91c87d7619fb7d2a937c28ffb61c518b528871
Kieran Tallinger <dellingerkieran@gmail.com>
2020-02-06 16:54:22 -0800

    Created the method renderGradeRow in the GradeTable class constructor

--- assets/components/grade-table.js
+++ assets/components/grade-table.js
@@ -1,6 +1,7 @@
 class GradeTable {
   constructor(tableElement, gradeTable){
     this.tableElement = tableElement;
+    this.onDeleteClick = null;
   }
   updateGrades(grades){
     var tableEl = this.tableElement;
@@ -23,5 +24,26 @@ class GradeTable {
   }
   onDeleteClick(deleteGrade) {
     this.deleteGrade = deleteGrade;
+  }
+  renderGradeRow(data,deleteGrade){
+    var newTableRow = document.createElement('tr');
+    var newTableDataName = document.createElement('td');
+    newTableDataName.textContent = data.name;
+    newTableRow.appendChild(newTableDataName);
+    var newTableDataCourse = document.createElement('td');
+    newTableDataCourse.textContent = data.course;
+    newTableRow.appendChild(newTableDataCourse);
+    var newTableDataGrade = document.createElement('td');
+    newTableDataGrade.textContent = data.grade;
+    newTableRow.appendChild(newTableDataGrade);
+    var newTableDataButtonSpot = document.createElement('td');
+    newTableRow.appendChild(newTableDataButtonSpot);
+    var newButton = document.createElement('button');
+    newButton.textContent = 'DELETE';
+    newButton.addEventListener('click', function(){
+      this.deleteGrade(data.id);
+    })
+    newTableDataButtonSpot.appendChild(newButton);
+    return newTableRow;
   }
 }
